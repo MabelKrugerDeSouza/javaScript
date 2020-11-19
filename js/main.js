a19faffd7b906952f175a61bc5b91aafc4b1d456
Mabel Kruger de Souza <mabelksouza@gmail.com>
2020-11-19 15:35:33 -0300

    Comandos básicos do java script

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Mabel Kruger de Souza";
+var idade = 17;
+var idade2 = 28;
+var frase = "Hoje o dia ta cansativo";
+//alert("Seja bem-vindx " + nome + " :)");
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase());
+alert(frase.replace("cansativo", "chato"));
